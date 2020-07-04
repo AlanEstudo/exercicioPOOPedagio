0e8b9256324ce198e98e69110772c284069eed58
AlanEstudo <alanjuniorestudo@gmail.com>
2020-07-04 07:02:18 -0300

    Criando Classe Localizacao

---
+++ src/Localizacao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alan <alanjuniorestudo@gmail.com>
+ */
+public class Localizacao {
+    private String rodovia ;
+    private float km;
+
+    public String getRodovia() {
+        return rodovia;
+    }
+
+    public void setRodovia(String rodovia) {
+        this.rodovia = rodovia;
+    }
+
+    public float getKm() {
+        return km;
+    }
+
+    public void setKm(float km) {
+        this.km = km;
+    }
+    
+    
+}
